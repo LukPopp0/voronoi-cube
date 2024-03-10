2c26ccec76872230317e1d1ed61a02f8b3c81ffb
Lukas Popp <luk.popp0@gmail.com>
2024-03-09 20:57:03 -0500

    Fix download button

--- src/components/settings/downloadButton.tsx
+++ src/components/settings/downloadButton.tsx
@@ -2,7 +2,7 @@ import { useCallback, useContext } from 'react';
 import { SceneContext } from '../../hooks/sceneContext';
 import { Group, Mesh, Scene } from 'three';
 import { STLExporter } from 'three/examples/jsm/exporters/STLExporter';
-import { CSG } from 'three-csg-ts';
+import { Brush, Evaluator, SUBTRACTION } from 'three-bvh-csg';
 
 const download = (filename: string, text: string) => {
   const element = document.createElement('a');
@@ -24,9 +24,27 @@ export const DownloadButton = () => {
     const innerCube = (scene as Scene).getObjectByName('innerCube');
     if (!voronoiCube || !innerCube) return;
     const group = new Group();
-    voronoiCube.children.forEach(part => {
-      if (part.type === 'Mesh') group.add(CSG.subtract(part as Mesh, innerCube as Mesh));
-    });
+    const evaluator = new Evaluator();
+    evaluator.attributes = ['position'];
+
+    const innerCubeBrush = new Brush((innerCube as Mesh).geometry.clone());
+
+    // Cut each cell with the inner cube
+    for (let i = 0; i < voronoiCube.children.length; ++i) {
+      const part = voronoiCube.children[i];
+      if (part.type === 'Mesh') {
+        // Create brush that gets cut and move by cell position
+        const cell = new Brush((part as Mesh).geometry.clone());
+        cell.geometry.translate(part.position.x, part.position.y, part.position.z);
+
+        const output = new Brush();
+
+        evaluator.evaluate(cell, innerCubeBrush, SUBTRACTION, output);
+
+        group.add(output);
+      }
+    }
+
     const data = new STLExporter().parse(group);
     download('voronoi.stl', data);
   }, [scene]);
